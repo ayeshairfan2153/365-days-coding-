806033aac3e417dc10c8b36a7f2ffae5ed3d01fd
Ayesha Irfan <46882258+ayeshairfan2153@users.noreply.github.com>
2023-01-17 23:48:43 +0500

    Add files via upload

---
+++ day9.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int n, reversed_number = 0, remainder;
+
+  cout << "Enter an integer: ";
+  cin >> n;
+
+  while(n != 0) {
+    remainder = n % 10;
+    reversed_number = reversed_number * 10 + remainder;
+    n /= 10;
+  }
+
+  cout << "Reversed Number = " << reversed_number;
+
+  
+
+
+    return 0;
+}
