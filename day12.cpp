7459c634be112455fdf7aeef74fc53daa9548ffa
Ayesha Irfan <46882258+ayeshairfan2153@users.noreply.github.com>
2023-01-20 23:06:47 +0500

    Add files via upload

---
+++ day12.cpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int rows;
+    cout << "Enter the number of rows : ";
+    cin >> rows;
+    cout << endl;
+ 
+    for (int i = 0; i < rows; i++)
+    {
+        int val = 1;
+        for (int j = 1; j < (rows - i); j++)
+        {
+            cout << "   ";
+        }
+        for (int k = 0; k <= i; k++)
+        {
+            cout << "      " << val;
+            val = val * (i - k) / (k + 1);
+        }
+        cout << endl << endl;
+    }
+    cout << endl;
+
+    return 0;
+}
